1a1e65214b9c1eb1b986a93c8c9e0d261573ea01
Tariq-Sekhri <tariqsekhri@gmail.com>
2025-09-21 01:39:12 -0400

    finish db.rs

--- src-tauri/src/db.rs
+++ src-tauri/src/db.rs
@@ -7,6 +7,12 @@ use rusqlite::{params, Connection, Result};
 const PATH_FOLDER: &str = "../data";
 const PATH: &str = "../data/app.db";
 
+pub struct Log {
+    pub id: i32,
+    pub app: String,
+    pub timestamp: u128,
+}
+
 pub fn open() -> Result<Connection, Box<dyn std::error::Error>> {
     fs::create_dir_all(PATH_FOLDER)?;
     Ok(Connection::open(PATH)?)
@@ -16,10 +22,10 @@ pub fn open() -> Result<Connection, Box<dyn std::error::Error>> {
 
 pub fn create_table(conn: &Connection) -> Result<()> {
     conn.execute(
-        "CREATE TABLE IF NOT EXISTS app (
-            id INTEGER PRIMARY KEY,
-            app TEXT,
-            timestamp INTEGER
+        "CREATE TABLE IF NOT EXISTS log (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            app TEXT NOT NULL,
+            timestamp TEXT NOT NULL
         )",
         [],
     )?;
@@ -31,14 +37,25 @@ pub fn drop_all()->std::io::Result<()>{
     Ok(())
 }
 
+pub fn insert_log(conn:&Connection, log:Log) ->Result<()>{
+    conn.execute(
+        "insert into log (app, timestamp) VALUES (?,?)" ,
+        params![log.app, log.timestamp.to_string()]
+    )?;
+    Ok(())
+}
 
-// pub fn idk(&conn:&Connection){
-//     conn.execute("INSERT INTO t (name) VALUES (?)", params!["Alice"])?;
-//     let mut stmt = conn.prepare("SELECT id, name FROM t")?;
-//     let rows = stmt.query_map([], |r| Ok((r.get::<_, i64>(0)?, r.get::<_, String>(1)?)))?;
-//     for row in rows {
-//         let (id, name) = row?;
-//         println!("{id}: {name}");
-//     }
-//     Ok(())
-// }
+pub fn get_logs(conn: &Connection) -> Result<Vec<Log>> {
+    let mut stmt = conn.prepare("SELECT id, app, timestamp FROM log")?;
+
+    let logs_iter = stmt.query_map([], |row| {
+        let timestamp_str: String = row.get(2)?;
+        Ok(Log {
+            id: row.get(0)?,
+            app: row.get(1)?,
+            timestamp: timestamp_str.parse::<u128>().unwrap(),
+        })
+    })?;
+    let logs: Vec<Log> = logs_iter.filter_map(|r| r.ok()).collect();
+    Ok(logs)
+}
