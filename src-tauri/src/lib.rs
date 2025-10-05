50fb3e70fb5591ef89a8a899ec6e96f1e3313414
Tariq-Sekhri <tariqsekhri@gmail.com>
2025-10-05 14:25:02 -0400

    init logger_service

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -16,7 +16,6 @@ use crate::db::log::JsonLog;
 
 
 
-// Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
 #[tauri::command]
 fn greet(name: &str) -> String {
     format!("Hello, {}! You've been greeted from Rust!", name)
