7bc3a0a4a30d91282f34600749d5c058e0a16333
Tariq-Sekhri <tariqsekhri@gmail.com>
2025-11-04 03:44:40 -0500

    Update cat_regex.rs

--- src-tauri/src/db/cat_regex.rs
+++ src-tauri/src/db/cat_regex.rs
@@ -1,7 +1,8 @@
 use serde::Serialize;
-use sqlx::{Error, SqlitePool};
+use sqlx::{Error, SqlitePool, FromRow};
+use crate::db::category::NewCategory;
 
-#[derive(Debug, Serialize)]
+#[derive(Debug, Serialize,FromRow)]
 pub struct CategoryRegex {
     id: i32,
     cat_id: i32,
@@ -23,5 +24,37 @@ pub async fn create_table(pool:&SqlitePool)->Result<(),Error>{
     regex    TEXT    not null,
     priority integer
 );").execute(pool).await?;
+    Ok(())
+}
+
+pub async fn insert(pool:&SqlitePool, new_category_regex: NewCategoryRegex)->Result<(),Error>{
+    sqlx::query("INSERT INTO category_regex (cat_id, regex, priority) values (?, ?, ?)")
+        .bind(new_category_regex.cat_id)
+        .bind(new_category_regex.regex)
+        .bind(new_category_regex.priority)
+        .execute(pool).await?;
+    Ok(())
+}
+
+pub async fn update_by_id(pool:&SqlitePool, cat_regex:CategoryRegex)->Result<(),Error>{
+    sqlx::query("update category_regex set cat_id= ?, regex=?, priority=? where id = ?")
+        .bind(cat_regex.cat_id)
+        .bind(cat_regex.regex)
+        .bind(cat_regex.priority)
+        .bind(cat_regex.id)
+        .execute(pool)
+        .await?;
+    Ok(())
+}
+
+pub async fn get_by_id(pool:&SqlitePool)->Result<Option<CategoryRegex>,Error>{
+    sqlx::query_as::<_, CategoryRegex>("").fetch_optional(pool).await
+}
+pub async fn get_all(pool:&SqlitePool)->Result<Vec<CategoryRegex>,Error>{
+    sqlx::query_as::<_, CategoryRegex>("select * from category_regex").fetch_all(pool).await
+}
+
+pub async fn delete_by_id(pool:&SqlitePool, id:i32)->Result<(),Error>{
+    sqlx::query("delete category category_regex where id=?").bind(id).execute(pool).await?;
     Ok(())
 }
