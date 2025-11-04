05920d4a07c4d54b5bfe96a49ad7a8f24932e82b
Tariq-Sekhri <tariqsekhri@gmail.com>
2025-11-04 17:44:33 -0500

    Implement singleton SqlitePool with OnceLock

--- src-tauri/src/db/mod.rs
+++ src-tauri/src/db/mod.rs
@@ -1,4 +1,5 @@
 use std::fs;
+use std::sync::OnceLock;
 use sqlx::{sqlite::SqlitePoolOptions, SqlitePool};
 
 pub mod log;
@@ -7,13 +8,22 @@ pub mod cat_regex;
 
 const PATH_FOLDER: &str = "../data";
 const PATH: &str = "../data/app.db";
-
+static POOL:OnceLock<SqlitePool> = OnceLock::new();
 pub fn drop_all() -> std::io::Result<()> {
     fs::remove_file(PATH)?;
     Ok(())
 }
+pub async fn get_pool()->Result<&'static SqlitePool, sqlx::Error>{
+    if let Some(pool) = POOL.get()  {
+        Ok(pool)
+    }else{
+        let pool = create_pool().await?;
+        let _ = POOL.set(pool).ok();
+        Ok(POOL.get().unwrap())
+    }
+}
 
-pub async fn create_pool() -> Result<SqlitePool, sqlx::Error> {
+ async fn create_pool() -> Result<SqlitePool, sqlx::Error> {
     let pool = SqlitePoolOptions::new()
         .max_connections(10)
         .connect(&format!("sqlite://{}", PATH))
--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -13,65 +13,37 @@ use chrono::{Local, TimeZone};
 use serde_json::to_string_pretty;
 use windows::Win32::Foundation::HWND;
 use windows::Win32::UI::WindowsAndMessaging as ws;
-
-
-
+use db::log::NewLog;
 
 #[tauri::command]
 fn greet(name: &str) -> String {
     format!("Hello, {}! You've been greeted from Rust!", name)
 }
 
-//
-//
-// fn generate_log()->Log{
-//     let hwnd:HWND = unsafe{ ws::GetForegroundWindow()};
-//
-//     let mut buf: [u16; 1024] = [0; 1024];
-//     let n = unsafe { ws::GetWindowTextW(hwnd, &mut buf) };
-//     let fore_ground_window = String::from_utf16_lossy(&buf[..n as usize]);
-//     let now = SystemTime::now();
-//
-//     println!("{}", fore_ground_window);
-//     println!("{}", now.duration_since(UNIX_EPOCH).unwrap().as_secs());
-//     Log::new(None,fore_ground_window, now.duration_since(UNIX_EPOCH).unwrap().as_secs())
-//  }
-// //TODO - make this a background process
-// fn background_process(){
-//     while true {
-//
-//     }
-//     let conn = match db::open(){
-//         Ok(conn)=>conn,
-//         Err(e)=> {eprintln!("Error connecting to database:{e}");return;}
-//     };
-//
-//     match  db::create_tables(&conn){
-//         Ok(())=>println!("Succsesfully created tables"),
-//         Err(e)=>eprintln!("error creating tables: {e}")
-//     };
-//     // let cat = Category::new(None, "Watching".to_string());
-//     // match category::insert(&conn,cat  ){
-//     //     Ok(cat)=>{println!("Successfully added log to logs table")},
-//     //     Err(e)=>{eprintln!("error adding log to logs table: {e}")}
-//     // }
-//
-//
-//     // match log::insert_log(&conn, generate_log()){
-//     //     Ok(())=>{println!("Successfully added log to logs table")}
-//     //     Err(e) =>{println!("error adding log to logs table: {e}")}
-//     // }
-//     db_to_json();
-// }
+
+
+fn generate_log()->NewLog{
+    let hwnd:HWND = unsafe{ ws::GetForegroundWindow()};
+
+    let mut buf: [u16; 1024] = [0; 1024];
+    let n = unsafe { ws::GetWindowTextW(hwnd, &mut buf) };
+    let fore_ground_window = String::from_utf16_lossy(&buf[..n as usize]);
+    let now = SystemTime::now();
+
+    println!("{}", fore_ground_window);
+    println!("{}", now.duration_since(UNIX_EPOCH).unwrap().as_secs());
+     NewLog{app:fore_ground_window, timestamp:now.duration_since(UNIX_EPOCH).unwrap().as_secs() as i64}
+ }
+
 // #[tauri::command]
-// fn db_to_json() -> String{
-//     let conn = match db::open(){
-//         Ok(conn)=>conn,
-//         Err(e)=> {return format!("Error connecting to database:{e}");}
+// async fn db_to_json() -> String{
+//     let pool = match db::get_pool().await {
+//         Ok(pool) => pool,
+//         Err(e) => return format!("Error connecting to database: {e}"),
 //     };
-//     let logs:Vec<Log>=match log::get_logs(&conn) {
-//         Ok( logs)=> logs,
-//         Err(e)=>{return format!("Error getting Logs table: {e}");}
+//     let logs: Vec<Log> = match log::get_all(&pool).await {
+//         Ok(logs) => logs,
+//         Err(e) => return format!("Error getting Logs table: {e}"),
 //     };
 //
 //
@@ -110,10 +82,8 @@ fn greet(name: &str) -> String {
 //     println!("db to json called");
 //     return json_db;
 // }
-//
-// fn timestamp_to_string(timestamp:u64)->String{
-//     Local.timestamp_opt(timestamp as i64, 0).single().unwrap().format("%Y-%m-%d %I:%M:%S %p").to_string()
-// }
+
+
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
