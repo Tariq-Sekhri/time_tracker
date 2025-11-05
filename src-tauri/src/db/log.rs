917465314a943f98837685f26c6e55a8edffc536
Tariq-Sekhri <tariqsekhri@gmail.com>
2025-11-04 22:56:43 -0500

    draft 1 background process

--- src-tauri/src/db/log.rs
+++ src-tauri/src/db/log.rs
@@ -3,12 +3,13 @@ use chrono::{Local, TimeZone};
 use sqlx::{SqlitePool, Error, FromRow};
 use std::time::{SystemTime, UNIX_EPOCH};
 
-#[derive(Debug, Serialize, FromRow)]
+#[derive(Debug, Serialize, FromRow,Clone)]
 pub struct Log {
-    pub id: i32,
+    pub id: i64,
     pub app: String,
     #[serde(serialize_with = "serialize_timestamp")]
     pub timestamp: i64,
+    pub duration:i64
 }
 
 #[derive(Debug, Serialize)]
@@ -35,7 +36,8 @@ pub async fn create_table(pool: &SqlitePool) -> Result<(), Error> {
         "CREATE TABLE IF NOT EXISTS logs (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
             app TEXT NOT NULL,
-            timestamp TEXT NOT NULL
+            timestamp INTEGER NOT NULL,
+            duration INTEGER NOT NULL default 0
         )"
     )
         .execute(pool)
@@ -43,16 +45,17 @@ pub async fn create_table(pool: &SqlitePool) -> Result<(), Error> {
     Ok(())
 }
 
-pub async fn insert(log: NewLog, pool: &SqlitePool) -> Result<(), Error> {
-    sqlx::query("INSERT INTO logs (app, timestamp) VALUES (?, ?)")
+pub async fn insert(pool: &SqlitePool,log: NewLog) -> Result<i64, Error> {
+   let result =  sqlx::query("INSERT INTO logs (app, timestamp) VALUES (?, ?)")
         .bind(log.app)
         .bind(log.timestamp.to_string())
         .execute(pool)
         .await?;
-    Ok(())
+
+    Ok(result.last_insert_rowid())
 }
 
-pub async fn delete_by_id(pool: &SqlitePool, id: i32) -> Result<(), Error> {
+pub async fn delete_by_id(pool: &SqlitePool, id: i64) -> Result<(), Error> {
     sqlx::query("DELETE FROM logs WHERE id = ?")
         .bind(id)
         .execute(pool)
@@ -61,8 +64,18 @@ pub async fn delete_by_id(pool: &SqlitePool, id: i32) -> Result<(), Error> {
 }
 
 pub async fn get_all(pool: &SqlitePool) -> Result<Vec<Log>, Error> {
-    sqlx::query_as::<_, Log>("SELECT id, app, timestamp FROM logs")
+    sqlx::query_as::<_, Log>("SELECT *  FROM logs")
         .fetch_all(pool)
         .await
 }
-
+pub async fn get_by_id(pool: &SqlitePool,id:i64 )->Result<Log, Error>{
+    sqlx::query_as::<_, Log>("select * from logs where id = ?")
+        .bind(id).fetch_one(pool).await
+}
+pub async fn increase_duration(pool: &SqlitePool, id: i64) -> Result<(), Error> {
+    sqlx::query("update logs set duration = duration + 1 where id = ?")
+        .bind(id)
+        .execute(pool)
+        .await?;
+    Ok(())
+}
