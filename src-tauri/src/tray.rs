89df74403f838c4c5a6866541cf6b782f0d80cc8
Tariq-Sekhri <tariqsekhri@gmail.com>
2025-11-07 23:13:29 -0500

    Add system tray functionality
    - Hide window to tray on close
    - Restore from tray
    - Quit option in menu

---
+++ src-tauri/src/tray.rs
@@ -0,0 +1,40 @@
+use tauri::{
+    image::Image,
+    tray::TrayIconBuilder,
+    Manager,
+    menu::{Menu, MenuItem},
+    AppHandle,
+    WindowEvent,
+};
+use std::fs;
+
+pub fn setup_tray(app: &AppHandle) -> Result<(), Box<dyn std::error::Error>> {
+    let show = MenuItem::with_id(app, "show", "Show", true, None::<String>)?;
+    let quit = MenuItem::with_id(app, "quit", "Quit", true, None::<String>)?;
+    let menu = Menu::with_items(app, &[&show, &quit])?;
+
+    TrayIconBuilder::new()
+        .icon(app.default_window_icon().unwrap().clone())
+        .menu(&menu)
+        .on_menu_event(|app, event| match event.id.as_ref() {
+            "quit" => {
+                app.exit(0);
+            }
+            "show" => {
+                if let Some(window) = app.get_window("main") {
+                    window.show().unwrap();
+                    window.set_focus().unwrap();
+                }
+            }
+            _ => {}
+        })
+        .build(app)?;
+    Ok(())
+}
+
+pub fn handle_window_event(window: &tauri::Window, event: &WindowEvent) {
+    if let WindowEvent::CloseRequested { api, .. } = event {
+        window.hide().unwrap();
+        api.prevent_close();
+    }
+}
