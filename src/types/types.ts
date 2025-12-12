d904d27ab473c959492ede4244b9b3d633d1c43f
Tariq-Sekhri <tariqsekhri@gmail.com>
2025-12-11 20:00:09 -0500

    added invokeWithResult

--- src/types/types.ts
+++ src/types/types.ts
@@ -1,18 +1,26 @@
+import {invoke} from "@tauri-apps/api/core";
+
+
 export type Result<D, E> =
     { success: true, data: D } |
     { success: false, error: E };
 
+export async function invokeWithResult<T>(
+    command: string,
+    args?: Record<string, any>
+): Promise<Result<T, AppError>> {
+    const res: T | AppError = await invoke(command, args);
+    if (res != null && typeof res == "object" && "type" in res) {
+        return {success: false, error: res};
+    }
+    return {success: true, data: res};
+}
 
 export type AppError =
     | { type: "Db"; data: string }
     | { type: "NotFound" }
     | { type: "Other"; data: string };
 
-export type Category = {
-    id: number,
-    name: string,
-    priority: number
-}
 
 export type CategoryRegex = {
     id: number,
